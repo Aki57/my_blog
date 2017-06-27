3e4d47e15ab3df538df561e333c5bc8c90e64114
hqh <1152042601@qq.com>
2017-06-27 21:23:22 +0800

    unstable
    still have some errors

--- article/models.py
+++ article/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Article(models.Model):
@@ -9,6 +10,11 @@ class Article(models.Model):
     category = models.CharField(max_length=50, blank=True)
     date_time = models.DateTimeField(auto_now_add=True)
     content = models.TextField(blank=True, null=True)
+
+    # 获取URL并转换成url的表示格式
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id': self.id})
+        return "http://127.0.0.1:8000%s" % path
 
     # python2使用__unicode__, python3使用__str__
     def __unicode__(self):
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -20,5 +20,5 @@ from article import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
-    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
 ]
